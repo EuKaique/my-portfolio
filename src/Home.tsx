31af3757dcca0f1eea5f934d8203b27b55a39345
Kaique <kaique.santos@duosystem.com.br>
2023-10-15 19:00:39 -0300

    adding scrollToTop

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,24 +8,55 @@ import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import { Socials } from "./components/Socials";
 import { useTheme } from "./store/useTheme";
-import { HomeContainer } from "./styles";
+import { HomeContainer, ScrollTop } from "./styles";
+import { PiArrowFatUpBold } from 'react-icons/pi'
+import { useEffect, useState } from "react";
 
 export default function Home(){
     const themeColor = useTheme();
 
+    const [isVisible, setIsVisible] = useState(false)
+
+    const handleScroll = () => {
+      if (window.pageYOffset > 200) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
+      }
+    }
+  
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+          window.removeEventListener('scroll', handleScroll)
+        };
+      }, [])
+
     return(
-        <HomeContainer themeColor={themeColor.light}>
-            <Aside>
-                <Info themeColor={themeColor.light}/>
-                <Contact themeColor={themeColor.light}/>
-                <Socials themeColor={themeColor.light}/>
-            </Aside>
-            <Menu>
-                <Experience themeColor={themeColor.light}/>
-                <Projects themeColor={themeColor.light}/>
-                <Education themeColor={themeColor.light}/>
-                <Skills themeColor={themeColor.light}/>
-            </Menu>
-        </HomeContainer>
+            <HomeContainer themeColor={themeColor.light}>
+                <Aside>
+                    <Info themeColor={themeColor.light}/>
+                    <Contact themeColor={themeColor.light}/>
+                    <Socials themeColor={themeColor.light}/>
+                </Aside>
+                <Menu>
+                    <Experience themeColor={themeColor.light}/>
+                    <Projects themeColor={themeColor.light}/>
+                    <Education themeColor={themeColor.light}/>
+                    <Skills themeColor={themeColor.light}/>
+                </Menu>
+                {isVisible && (
+                    <ScrollTop themeColor={themeColor.light} onClick={scrollToTop}>
+                        <PiArrowFatUpBold />
+                    </ScrollTop>
+                )}
+            </HomeContainer>
     )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -12,3 +12,14 @@ export const HomeContainer = styled.main<{ themeColor: boolean }>`
         px-2
     `}
 `;
+
+export const ScrollTop = styled.button<{ themeColor: boolean }>`
+  position: fixed;
+  bottom: 3%;
+  right: 3%;
+  border-radius: 50%;
+  padding: 0.65rem;
+  background-color: ${(props) => (props.themeColor ? "#232339" : "#EBEBEB")};
+  color: ${(props) => (props.themeColor ? "#EBEBEB" : "#232339")};
+  font-size: 1.35rem;
+`
